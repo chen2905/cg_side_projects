74ff7619f438f141a6bbfbe81b983745f35b205e
Max <maxlin2905@gmail.com>
2021-12-24 14:21:17 +1100

    221

--- todelete/test_git_branch.js
+++ todelete/test_git_branch.js
@@ -10,3 +10,17 @@ console.log("changed after pushing to github")
 console.log("changed after merging")
 
 console.log("changed after pushing to github")
+
+
+console.log("changed after merging")
+
+console.log("changed after pushing to github")
+
+console.log("changed after merging")
+
+console.log("changed after pushing to github")
+
+
+console.log("changed after merging")
+
+console.log("changed after pushing to github")
